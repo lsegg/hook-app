056ca14b051c440e2d1d0531636d52c7f42bddf7
Lucia <luciaseggiaro@gmail.com>
2024-09-16 13:57:45 +0200

    useRef

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -2,12 +2,13 @@
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { SimpleFormWithCustomHook } from "./02-useEffect/SimpleFormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 
 export const HooksApp = () => {
   return (
     <div>
-      <MultipleCustomHooks />
+      <FocusScreen />
     </div>
   );
 };
